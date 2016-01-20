25cce03759567da3b13171146e9c08a29200d049
Tom Auty <tauty@moneylion.com>
2016-01-19 21:13:01 -0500

    Apply patch from kauffecup/react-native-chart, and update package.json

--- Chart.ios.js
+++ Chart.ios.js
@@ -1,65 +1,60 @@
 'use strict';
 
 var React = require('react-native');
-var PropTypes = require('ReactPropTypes');
-var ReactNativeViewAttributes = require('ReactNativeViewAttributes');
-var createReactNativeComponentClass = require('createReactNativeComponentClass');
-var StyleSheet = require('StyleSheet');
-var NativeMethodsMixin = require('NativeMethodsMixin');
-var flattenStyle = require('flattenStyle');
+var {
+    PropTypes,
+    StyleSheet,
+    requireNativeComponent
+} = React;
 var merge = require('merge');
-var { View } = React;
 
-var RNChartView = createReactNativeComponentClass({
-    validAttributes: merge(ReactNativeViewAttributes.UIView, {
-        chartData:true,
-        verticalGridStep:true,
-        animationDuration:true,
+/** A native reference to the chart view */
+var CHART_REF = 'chart';
 
-        showGrid:true,
-        gridColor:true,
-        gridLineWidth:true,
+/** The native chart view */
+var RNChartView = requireNativeComponent('RNChartView', RNChart);
 
-        showAxis:true,
-        showXAxisLabels:true,
-        showYAxisLabels:true,
-        xLabels:true,
-        xAxisTitle:true,
-        yAxisTitle:true,
-        axisTitleColor:true,
-        axisTitleFontSize:true,
-        axisColor:true,
-        axisLineWidth:true,
-
-        labelFontSize:true,
-        labelTextColor:true,
-
-        chartTitle:true,
-        chartTitleColor:true,
-        chartFontSize:true
-    }),
-    uiViewClassName: 'RNChartView'
+/** A base styles object that the user can override by passing in their own styles */
+var styles = StyleSheet.create({
+    base: {}
 });
 
+/** Our bridge component */
 var RNChart = React.createClass({
-    mixins: [NativeMethodsMixin],
-
-    viewConfig: {
-        uiViewClassName: 'UIView',
-        validAttributes: ReactNativeViewAttributes.UIView
+    propTypes: {
+        // TODO: define what these objects look like
+        chartData: PropTypes.array.isRequired,
+        xLabels: PropTypes.array.isRequired,
+        animationDuration: PropTypes.number,
+        showGrid: PropTypes.bool,
+        verticalGridStep: PropTypes.number,
+        // TODO: allow strings and use processColor to convert
+        gridColor: PropTypes.number,
+        gridLineWidth: PropTypes.number,
+        showAxis: PropTypes.bool,
+        showXAxisLabels: PropTypes.bool,
+        showYAxisLabels: PropTypes.bool,
+        axisLineWidth: PropTypes.number,
+        labelFontSize: PropTypes.number,
+        // TODO: allow strings and use processColor to convert
+        labelTextColor: PropTypes.number
     },
 
+    /** Pass the props to the native component */
+    setNativeProps(props) {
+        this.refs[CHART_REF].setNativeProps(props);
+    },
+
+    /** Render the native component with the correct props */
     render() {
+        var style = [styles.base, this.props.style];
         var nativeProps = merge(this.props, {
-            style: this.props.style,
+            style,
             chartData: this.props.chartData
         });
 
-        return <RNChartView {... nativeProps} />
+        return <RNChartView ref={CHART_REF} {... nativeProps} />;
     }
-});
-
-var styles = StyleSheet.create({
 });
 
 module.exports = RNChart;
