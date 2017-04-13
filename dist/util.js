3f9c8036b62baad53ce9e8717e331acf0e54c2f6
Tom Auty <tom.auty@gmail.com>
2017-04-13 10:53:32 -0400

    Clean it up...

--- dist/util.js
+++
@@ -1,9 +0,0 @@
-Object.defineProperty(exports,"__esModule",{value:true});exports.
-
-uniqueValuesInDataSet=uniqueValuesInDataSet;function uniqueValuesInDataSet(data){
-return data.reduce(function(result,d){
-if(result.some(function(p){return p[1]===d[1];}))return result;
-result.push(d);
-return result;
-},[]);
-}
--- src/util.js
+++
@@ -1,9 +0,0 @@
-/* @flow */
-type Pair = [number, number];
-export function uniqueValuesInDataSet(data : Pair[]) : Pair[] {
-	return data.reduce((result : Pair[], d : Pair) => {
-		if (result.some(p => p[1] === d[1])) return result;
-		result.push(d);
-		return result;
-	}, []);
-}
